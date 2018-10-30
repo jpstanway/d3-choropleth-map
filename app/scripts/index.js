13da18ef135db155ede2eccb4c15756570d8a8f3
Jordan S <jpstanway@gmail.com>
2018-10-29 18:49:45 -0700

    implement topojson and draw state lines

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,25 +1,41 @@
-// variables for data request
+
+// urls containing json data
 const URLs = [
     'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/for_user_education.json',
     'https://raw.githubusercontent.com/no-stack-dub-sack/testable-projects-fcc/master/src/data/choropleth_map/counties.json'
 ];
-const json = {};
 const dataset = [];
-let req = new XMLHttpRequest();
 
-// data request loop
-for(let i = 0; i < URLs.length; i++) {
-    req.open('GET', URLs[i], true);
-    req.onreadystatechange = function() {
-        console.log(req.readyState);
-        if(req.readyState === req.DONE && req.status === 200) {
-            json[i] = JSON.parse(req.responseText);
-        }
-    }
-    req.send();
-}
+URLs.forEach((url) => {
+    dataset.push(d3.json(url));
+});
 
-// data received
-req.onload = function() {
+Promise.all(dataset).then((data) => {
 
-};
+    // declare dimensions
+    const width = 1080;
+    const height = 600;
+
+    // set up svg area
+    const svg = d3.select('#container')
+                    .append('svg')
+                    .attr('width', width)
+                    .attr('height', height)
+                    .append('g')
+                    .attr('transform', 'translate(0, 0)');
+
+    // set up path
+    /*const projection = d3.geoMercator()  
+                         .translate([width, height])
+                         .scale(100);*/
+
+    const path = d3.geoPath();     
+    const states = topojson.feature(data[1], data[1].objects.states).features;
+    
+    svg.selectAll('path')
+       .data(states)
+       .enter()
+       .append('path')
+       .attr('class', 'states')
+       .attr('d', path);         
+});
