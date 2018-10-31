738f66c7772f262b0410072909ed601491fc6d94
Jordan S <jpstanway@gmail.com>
2018-10-31 13:41:00 -0700

    create tooltip displaying county and state names as well as education rate

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -16,13 +16,30 @@ Promise.all(dataset).then((data) => {
     const width = 1080;
     const height = 600;
 
+    // create tooltip for individual counties
+    const tip = d3.tip()
+                  .attr('class', 'd3-tip')
+                  .attr('id', 'tooltip')
+                  .html((d) => {
+                    const loc = connectData(d.id, 'location');
+                    const rate = connectData(d.id, 'percentage');
+
+                    d3.select('#tooltip').attr('data-education', rate);
+
+                    return `
+                        <p>${loc}</p>
+                        <p>${rate}%</p>
+                    `;
+                  });         
+
     // set up svg area
     const svg = d3.select('#container')
                     .append('svg')
                     .attr('width', width)
                     .attr('height', height)
                     .append('g')
-                    .attr('transform', 'translate(0, 0)');
+                    .attr('transform', 'translate(0, 0)')
+                    .call(tip);
 
     // draw state and county lines using path
     const path = d3.geoPath();     
@@ -38,22 +55,26 @@ Promise.all(dataset).then((data) => {
 
     // create function to match data
     function connectData(id, dataToRetrieve) {
-        let countyData = data[0].filter((data) => id === data.fips);
-        countyData = countyData[0].bachelorsOrHigher;
+        const countyData = data[0].filter((data) => id === data.fips);
+        const percentage = countyData[0].bachelorsOrHigher;
+        const location = `${countyData[0].area_name}, ${countyData[0].state}`;
 
         if(dataToRetrieve === 'percentage') {
-            return countyData;
+            return percentage;
         } else if(dataToRetrieve === 'color') {
             for(let i = 0; i < colors.length; i++) {
-                if(countyData >= colors[i][0]) {
+                if(percentage >= colors[i][0]) {
                     return colors[i][1];
                 }
             }
+        } else if (dataToRetrieve === 'location') {
+            return location;
         } else {
             return false;
         }
     }
 
+    // draw counties and states
     svg.selectAll('path')
        .data(counties)
        .enter()
@@ -62,7 +83,9 @@ Promise.all(dataset).then((data) => {
        .attr('d', path)
        .attr('data-fips', (d) => d.id)
        .attr('data-education', (d) => connectData(d.id, 'percentage'))
-       .style('fill', (d) => connectData(d.id, 'color'));    
+       .style('fill', (d) => connectData(d.id, 'color'))
+       .on('mouseover', tip.show)
+       .on('mouseout', tip.hide);    
     
     svg.selectAll('path')
        .data(states)
